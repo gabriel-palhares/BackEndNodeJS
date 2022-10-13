c1246dc1afeaec7f908f55d516c0d08932e070a9
gabriel-palhares <gabrielpalharesdev@gmail.com>
2022-10-13 11:58:10 -0300

    introducao a erros

---
+++ JavaScript - tipos, variaveis e funcoes/erros.js
@@ -0,0 +1,5 @@
+const numero = 1;
+// const minhaVar = "oi";
+console.log(minhaVar);
+
+// console.log e console.error, aprensentação das sintaxes e ênfase na importância de ler as documentações de cada ferramenta
